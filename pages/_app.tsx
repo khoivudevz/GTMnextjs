64db3fcee033853c99100774ac9bbc669da26020
khoivudevz <khoivudev@gmail.com>
2022-11-24 14:55:18 +0700

    add GTM

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import TagManager, {TagManagerArgs} from "react-gtm-module"
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
+const gtmId = "GTM-NGH4JNT"
+
+const tagManagersArgs: TagManagerArgs ={
+  gtmId
+}
+
+useEffect(() =>{
+  TagManager.initialize(tagManagersArgs)
+},[])
+
   return <Component {...pageProps} />
 }
---
+++ pages/list/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export interface App {}
+
+export default function List(props: App) {
+  return <div>List</div>
+}
