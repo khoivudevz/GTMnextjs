22f3e8ce582413889f4e79760c2258882193342a
khoivudevz <khoivudev@gmail.com>
2022-11-24 15:14:38 +0700

    fix script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import TagManager, {TagManagerArgs} from "react-gtm-module"
 import { useEffect } from 'react';
+import TagManager from 'react-gtm-module';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const tagManagerArgs = {
-    gtmId: 'GTM-NGH4JNT'
-}
 
-TagManager.initialize(tagManagerArgs)
-
-
-
-// const gtmId = "GTM-NGH4JNT"
-
-// const tagManagersArgs: TagManagerArgs ={
-//   gtmId
-// }
-
-// useEffect(() =>{
-//   TagManager.initialize(tagManagersArgs)
-// },[])
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-NGH4JNT' });
+}, []);
 
   return <Component {...pageProps} />
 }
